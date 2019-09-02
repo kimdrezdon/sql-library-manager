15ac9fa4876b46c7906693def426f5e765f15593
Kim Drezdon <kdrezdon@gmail.com>
2019-09-02 09:58:55 -0500

    Add route for new book creation

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 const express = require("express");
 const app = express();
 
+//body parser
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false }));
+
 // imports the database from index.js
 const db = require('./db');
 
@@ -11,43 +15,8 @@ app.set("view engine", "pug");
 // destructures the Book model imported from db.models
 const { Book } = db.models;
 
-// async IIFE
-(async () => {
-    // sync all tables. force:true drops the table that exists each time the app is started and recreates it from the model definition
-    await db.sequelize.sync({ force: true });
-    try { 
-        // waits until all promises are returned by the Model.create() method are fulfilled
-        await Promise.all([
-            Book.create({
-                title: "Harry Potter and the Philosopher's Stone",
-                author: 'J.K. Rowling',
-                genre: 'Fantasy',
-                year: 1997
-            }),
-            Book.create({
-                title: "Harry Potter and the Chamber of Secrets",
-                author: 'J.K. Rowling',
-                genre: 'Fantasy',
-                year: 1998
-            }),
-            Book.create({
-                title: "The Last Time I Lied",
-                author: 'Riley Sager',
-                genre: 'Mystery',
-                year: 2018
-            })
-        ]);
-    } catch (error) {
-        if (error.name === 'SequelizeValidationError') {
-            // maps the errors array to display all the error messages
-            const errors = error.errors.map(err => err.message);
-            console.error('Validation errors: ', errors);
-        } else {
-            // rethrows other types of errors caught by catch (general errors, record missing, unforseen errors)
-            throw error;
-        }
-    }
-} ) ();
+// sync all tables. force:true drops the table that exists each time the app is started and recreates it from the model definition
+db.sequelize.sync();
 
 //Serve static files
 app.use("/static", express.static("public"));
--- routes/books.js
+++ routes/books.js
@@ -5,32 +5,49 @@ const router = express.Router();
 //Book model
 const { Book } = require("../db").models;
 
-//error handling middleware
-function asyncHandler (cb) {
-    return async (req, res, next) => {
-        try {
-            await cb (req, res, next);
-        } catch (err) {
-            res.render('error', {error: err, pageTitle: "Server Error"});
-        }
-    }
-}
-
 //GET /books - Display the full list of books 
-router.get('/', asyncHandler ( async (req, res) => {
-    const books = await Book.findAll();
-    res.render('index', {
-        pageTitle: "Books", 
-        books: books
-    });
-}));
+router.get('/', async (req, res, next) => {
+    try {
+        const books = await Book.findAll();
+        res.render('index', {
+            pageTitle: "Books", 
+            books: books
+        });
+    } catch (err) {
+        res.render('error', {error: err, pageTitle: "Server Error"});
+    }
+});
 
 //GET /books/new - Display the create new book form
-router.get('/new', asyncHandler ( async (req, res) => {
-    res.render('new-book', {
-        book: Book.build(), 
-        pageTitle: "New Book"
-    });
-}));
+router.get('/new', async (req, res, next) => {
+    try {
+        res.render('new-book', {
+            book: Book.build(), 
+            pageTitle: "New Book"
+        });
+    } catch (err) {
+        res.render('error', {error: err, pageTitle: "Server Error"});
+    }
+});
+
+//POST /books/new - Posts a new book to the database
+router.post('/new', async (req, res, next) => {
+    try {
+        const book = await Book.create(req.body);
+        console.log(book);
+        res.redirect('/');
+    } catch (err) {
+        if(err.name === "SequelizeValidationError") {
+            console.log('Sequelize Validation Error thrown');
+            res.render('new-book', {
+                book: Book.build(req.body),
+                pageTitle: "New Book",
+                errors: err.errors
+            })
+        } else {
+            res.render('error', {pageTitle: "Server Error"});
+        }
+    }
+});
 
 module.exports = router;
