b8fe355080773906e37c5592df8368911660fce9
Kim Drezdon <kdrezdon@gmail.com>
2019-09-21 15:06:44 -0500

    Move error route to app.js

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,4 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', (req, res) => res.redirect("/books"));
 
-//Error route
-router.use((req, res, next) => {
-    res.render('page-not-found', {pageTitle: "Page Not Found"});
-});
-
 module.exports = router;
