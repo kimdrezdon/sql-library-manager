c38c0df24fa56bbabcaf04c514fe788dbce33113
Kim Drezdon <kdrezdon@gmail.com>
2019-09-21 14:55:03 -0500

    Add error route for non-existent routes

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,9 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', (req, res) => res.redirect("/books"));
 
+//Error route
+router.use((req, res, next) => {
+    res.render('page-not-found', {pageTitle: "Page Not Found"});
+});
+
 module.exports = router;
